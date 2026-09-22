fe33e77df154e26f154fe03fd4570afca1e66829
JoaoIto <joaovictorpfr@gmail.com>
2026-09-22 14:37:24 -0300

    linha teste

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -15,7 +15,6 @@ export async function login(formData: FormData) {
     }
 
     const supabase = await createClient()
-
     const { error } = await supabase.auth.signInWithPassword({
       email,
       password,
